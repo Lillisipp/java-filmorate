75db297d2f8a40e435a9305ce28fab0b1ec66f3a
Nastya <Lillisip@ya.ru>
2025-06-28 15:34:09 +0300

    готовый2

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,9 +1,10 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class ValidationException extends RuntimeException{
+public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
     }
+
     public ValidationException(String message, Throwable cause) {
         super(message, cause);
     }
