e68e65a6657c8ecf131442da9842e9be10887609
Nastya <Lillisip@ya.ru>
2025-06-28 15:38:03 +0300

    готовый3

--- src/main/java/ru/yandex/practicum/filmorate/utils/Utils.java
+++ src/main/java/ru/yandex/practicum/filmorate/utils/Utils.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.utils;
 import java.util.Map;
 
 public class Utils {
-    public static int getNextId(Map<Integer, ?> map){
+    public static int getNextId(Map<Integer, ?> map) {
         return map.keySet().stream()
                 .mapToInt(id -> id)
                 .max()
