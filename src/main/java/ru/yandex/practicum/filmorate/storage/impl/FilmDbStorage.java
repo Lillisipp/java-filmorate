faf51f0c9b5d9063a1b04f75e675850d4ea659da
Lillisip <Lillisip@yandex.ru>
2025-08-06 22:46:48 +0300

    FilmDbStorage complit

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/FilmDbStorage.java
@@ -1,12 +1,12 @@
 package ru.yandex.practicum.filmorate.storage.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.repository.mappers.FilmRowMapper;
@@ -28,6 +28,26 @@ public class FilmDbStorage implements FilmStorage {
     private static final String FIND_ALL_QUERY = "SELECT * FROM films";
     private static final String SAVE_FILM = "INSERT INTO films(name, description, releaseDate, duratifilm, mpaRating) VALUES (?,?,?,?,?)";
     private static final String FIND_BY_ID_QUERY = "SELECT * FROM films WHERE film_id = ?";
+    private static final String UPDATE_BY_FILM = """
+            UPDATE films
+            SET name=?,
+             description=?,
+             releaseDate=?,
+             duratifilm=?,
+             mpaRating=?
+             WHERE film_id=?
+            """;
+    private static final String LIKE_FILM = "INSERT INTO film_likesfilm_id, user_id) VALUES (?,?)";
+    private static final String REMOVE_LIKE = "DELETE From film_likesfilm_id WHERE film_id=? AND user_id=?";
+    private static final String GET_TOP_LIKE_FILM = """
+SELECT f.* FROM films f
+LEFT JOIN film_likes l ON f.film_id = l.film_id
+             GROUP BY f.film_id
+             ORDER BY COUNT(l.user_id) DESC
+             LIMIT ?
+""";
+    private static final String CHECK_EXIST = "SELECT COUNT(*) FROM films WHERE film_id = ?";
+
 
     @Override
     public Collection<Film> getFilms() {
@@ -67,29 +87,46 @@ public class FilmDbStorage implements FilmStorage {
         return film;
     }
 
-
     @Override
     public Film update(Film newFilm) {
-        return null;
+        int rows = jdbc.update(UPDATE_BY_FILM,
+                newFilm.getName(),
+                newFilm.getDescription(),
+                Date.valueOf(newFilm.getReleaseDate()),
+                (int) newFilm.getDuration().toMinutes(),
+                newFilm.getMpa().getId(),
+                newFilm.getId()
+        );
+        if (rows == 0) {
+            throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
+        }
+        jdbc.update("DELETE FROM film_genres WHERE film_id = ?", newFilm.getId(), newFilm.getId());
+        for (Genre g : newFilm.getGenres()) {
+            jdbc.update("INSERT INTO film_genres(film_id, genre_id) VALUES (?,?)",
+                    newFilm.getId(), g.getId());
+        }
+        return newFilm;
     }
 
     @Override
     public void likeFilm(int filmId, int userId) {
+        jdbc.update(LIKE_FILM, filmId, userId);
 
     }
 
     @Override
     public void removeLikeFilm(int filmId, int userId) {
-
+        jdbc.update(REMOVE_LIKE, filmId, userId);
     }
 
     @Override
     public List<Film> topLikeFilm(int count) {
-        return List.of();
+        return jdbc.query(GET_TOP_LIKE_FILM, mapper, count);
     }
 
     @Override
     public boolean exist(Film film) {
-        return false;
+        Integer count = jdbc.queryForObject(CHECK_EXIST, Integer.class, film);
+        return count != null && count > 0;
     }
 }
