c3409ba8b242968b92201d34d8e6c612ee629eee
Lillisip <Lillisip@yandex.ru>
2025-08-11 22:33:15 +0300

    нет тестов БД работает

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO mpa_rating (mpa_id, name) VALUES
+MERGE INTO mpa_rating (mpa_id, name) VALUES
   (1,'G'), (2,'PG'), (3,'PG-13'), (4,'R'), (5,'NC-17');
 
-INSERT INTO genres (genre_id, name) VALUES
+MERGE INTO genres (genre_id, name) VALUES
   (1,'Комедия'), (2,'Драма'), (3,'Боевик'), (4,'Триллер'), (5,'Фантастика');
